c883bc2635b8267ac108596830054f4964312260
Emanuel <115133403+WillAvatec@users.noreply.github.com>
2023-06-16 22:19:25 -0500

    feat: return levels data from db with firestore

--- firebase.ts
+++ firebase.ts
@@ -1,11 +1,10 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
+import { collection, getDocs, getFirestore } from "firebase/firestore/lite";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDKb2lnD_PxDaF8YKuHRWxUF0zjEWVXU2Q",
   authDomain: "the-great-seek.firebaseapp.com",
@@ -13,11 +12,26 @@ const firebaseConfig = {
   storageBucket: "the-great-seek.appspot.com",
   messagingSenderId: "596301377756",
   appId: "1:596301377756:web:ba4eb3bce483d246c84419",
-  measurementId: "G-9GHJZLKD54"
+  measurementId: "G-9GHJZLKD54",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-export {app, analytics}
+// Reference to the dataBase
+
+const db = getFirestore(app);
+
+// Function that returns level data
+
+async function getLevels() {
+  const levelCollection = collection(db, "Levels");
+  const levelSnapshot = await getDocs(levelCollection);
+  const levelData = levelSnapshot.docs.map((doc) => {
+    return doc.data();
+  });
+  return levelData;
+}
+
+export { app, analytics, db, getLevels };
